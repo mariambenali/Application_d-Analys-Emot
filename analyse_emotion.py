41621b894e1ed0806482ea15cb8a1d6d591f3baf
mariambenali <miriam.bena@gmail.com>
2025-11-17 15:03:45 +0100

    setup file and add file analyse emotion that contain model hugginFace

---
+++ analyse_emotion.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+API_URL = "https://router.huggingface.co/hf-inference/models/nlptown/bert-base-multilingual-uncased-sentiment"
+headers = {
+    "Authorization": f"Bearer {os.environ['HF_TOKEN']}",
+}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+output = query({
+    "inputs": "I like you. I love you",
+})
+
+print(output)
